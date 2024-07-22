b963d1322a937d52548dc21b82f302a7b0c33284
BugaevAV <iustusvir@gmail.com>
2024-07-22 23:49:53 +0300

    create connection for hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.util.Properties;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.SessionFactory;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.HibernateException;
+
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/first_db";
@@ -20,5 +31,32 @@ public class Util {
             e.printStackTrace();
         }
         return conn;
+    }
+
+    public static SessionFactory getConnectionHibernate() {
+        SessionFactory sessionFactory = null;
+        try {
+            Configuration config = getConfiguration();
+            config.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(config.getProperties()).build();
+            sessionFactory = config.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
+
+    private static Configuration getConfiguration() {
+        Configuration config = new Configuration();
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, URL);
+        properties.put(Environment.USER, USER);
+        properties.put(Environment.PASS, PASSWORD);
+        properties.put(Environment.SHOW_SQL, "true");
+        config.setProperties(properties);
+        return config;
     }
 }
