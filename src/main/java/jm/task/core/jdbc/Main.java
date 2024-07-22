add7fa505270f72ae022b1bfc50e35151ab0485f
BugaevAV <iustusvir@gmail.com>
2024-07-22 11:04:47 +0300

    create controller and show operations

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Пётр", "Чаадаев", (byte) 62);
+        userService.saveUser("Николай", "Чернышевский", (byte) 61);
+        userService.saveUser("Константин", "Циолковский", (byte) 78);
+        userService.saveUser("Владимир", "Вернадский", (byte) 82);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
