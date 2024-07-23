db0d6a497d3d1b1ba8ebfc328fb72c1f870fde13
BugaevAV <iustusvir@gmail.com>
2024-07-23 10:59:38 +0300

    remove calls for previous task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,15 +7,6 @@ import jm.task.core.jdbc.service.UserViaHibernateServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Пётр", "Чаадаев", (byte) 62);
-        userService.saveUser("Николай", "Чернышевский", (byte) 61);
-        userService.saveUser("Константин", "Циолковский", (byte) 78);
-        userService.saveUser("Владимир", "Вернадский", (byte) 82);
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
 
         UserService hibernatUserService = new UserViaHibernateServiceImpl();
         hibernatUserService.createUsersTable();
